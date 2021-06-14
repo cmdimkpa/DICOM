061be9c252ab1cf2d051a7bdb465989f169244f6
Monty Dimkpa <cmdimkpa@gmail.com>
2021-06-14 19:06:29 +0100

    Update data_robot.py

--- data_robot.py
+++ data_robot.py
@@ -91,7 +91,6 @@ class Listener:
         for __file in ready:
             try:
                 os.remove(__file)
-                self.to_clean = [i for i in self.to_clean if i[0] != __file]
             except:
                 pass
     def scan(self):
